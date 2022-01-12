f0c711dc4e479942a00f92f4aca4b85af39b67aa
cubeiplkumar <emailanilkumarm@gmail.com>
2022-01-12 08:50:34 +0530

    new changes for feature 200

--- SpringBootApp.java
+++ SpringBootApp.java
@@ -31,6 +31,17 @@ public static void sampleMethod(int a, int b) throws ArithmeticException{
 public static void main (String[] args){
     DemoClass.sampleMethod(4,2);     
  }
+//feature 200 changes done by Mahender
+public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
 
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+  }
 
 }
