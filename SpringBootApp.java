424eb5c3577d1f8fe5a75617378b15b34f4f2214
cubeiplkumar <emailanilkumarm@gmail.com>
2022-01-12 08:27:46 +0530

    hdfc301 changes done by vivek

--- SpringBootApp.java
+++ SpringBootApp.java
@@ -21,12 +21,10 @@ class Main {
     catch(Exception e) {
       e.getStackTrace();
     }
-//new code changes done by Mahender for the userstory hdfc300
-// reads all data from input.txt
-      sourceFile.read(array);
-
-      // writes all data to newFile
-      destFile.write(array);
-      System.out.println("The input.txt file is copied to newFile.");
-  }
+//new code changes done by vivek for the user story hdfc301
+public static void sampleMethod(int a, int b) throws ArithmeticException{
+        System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   }
 }
