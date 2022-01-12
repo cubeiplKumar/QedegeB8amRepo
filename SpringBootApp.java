e93049cb2b7f8babe710cd1a2fb2685afddeddd8
cubeiplkumar <emailanilkumarm@gmail.com>
2022-01-12 08:42:58 +0530

    hdfc 301 new changes

--- SpringBootApp.java
+++ SpringBootApp.java
@@ -27,4 +27,10 @@ public static void sampleMethod(int a, int b) throws ArithmeticException{
         int c = a/b; //  int c = 4/2 which gets calculated as 2 
         System.out.println("c:"+c); // c: 2
    }
+   
+public static void main (String[] args){
+    DemoClass.sampleMethod(4,2);     
+ }
+
+
 }
